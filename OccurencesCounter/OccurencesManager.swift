320f35b7ee6ddc644c3474c97f7c258c2ef80ccf
Michele Franco <michele.franco23@gmail.com>
2019-02-23 21:36:31 +0100

    ✅ TDD-testEmptyString

--- OccurencesCounter/OccurencesManager.swift
+++ OccurencesCounter/OccurencesManager.swift
@@ -2,10 +2,6 @@ import Foundation
 
 struct OccurencesManager {
     func occurences(from: String) -> Set<WordOccurence> {
-        let wordOccurence = WordOccurence(word: "aaaa", occurence: 2)
-        var set = Set<WordOccurence>()
-        set.insert(wordOccurence)
-        
-        return set
+        return Set<WordOccurence>()
     }
 }
