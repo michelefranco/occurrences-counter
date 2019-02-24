87233abf4f8bc2e24616633926e3fac821b97f98
Michele Franco <michele.franco23@gmail.com>
2019-02-24 03:30:13 +0100

    Prime number suite added

---
+++ OccurencesCounterTests/PrimeNumberTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+class PrimeNumberTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
