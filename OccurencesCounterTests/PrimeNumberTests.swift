624e93b705107468196bb4e92b3437d7944a727d
Michele Franco <michele.franco23@gmail.com>
2019-02-24 03:32:31 +0100

    ❌ TDD - testPrimeNumber_2

--- OccurencesCounterTests/PrimeNumberTests.swift
+++ OccurencesCounterTests/PrimeNumberTests.swift
@@ -1,9 +1,13 @@
 import XCTest
+@testable import OccurencesCounter
 
 class PrimeNumberTests: XCTestCase {
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testPrimeNumber_2() {
+        let input = 2
+        let result = input.isPrime
+        
+        let expected = true
+        XCTAssertEqual(result, expected, "result is wrong for input: \(input)")
     }
 }
